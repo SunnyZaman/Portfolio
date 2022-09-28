4c6c1fa63450a3af0eab72a39c84a1b0a4853200
Sunny Zaman <sunnyjzaman@hotmail.com>
2022-09-28 12:39:35 -0400

    get github repos

--- utils/fetchGitHubRepos.ts
+++ utils/fetchGitHubRepos.ts
@@ -1,9 +1,8 @@
-import { Experience } from "../typings";
-
 export const fetchGitHubRepos = async(hostname:string | undefined)=>{
     const res = await fetch(`http://${hostname}/api/getGitHubRepos`);
     const data = await res.json();
-    const repos: Experience[] = data.user.starredRepositories.edges
+    const { user } = data;
+  const repos = user.starredRepositories.edges
     .map(({ node }: any) => node);
     return repos;
 }
