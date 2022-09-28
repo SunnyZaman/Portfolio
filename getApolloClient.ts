1ede2bc2abe8028583bf82b7b0ac693f9384a55c
Sunny Zaman <sunnyjzaman@hotmail.com>
2022-09-28 16:20:06 -0400

    token

--- getApolloClient.ts
+++ getApolloClient.ts
@@ -10,7 +10,7 @@ export const getApolloClient = () => {
     return {
       headers: {
         ...headers,
-        authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+        authorization: `token ${process.env.GITHUB_ACCESS_TOKEN}`,
       },
     };
   });
