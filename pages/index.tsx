564ebfe03a01768a5e91b589d2fe679171da515d
Sunny Zaman <sunnyjzaman@hotmail.com>
2022-09-29 11:09:33 -0400

    dev

--- pages/index.tsx
+++ pages/index.tsx
@@ -108,7 +108,7 @@ Props) => {
       );
       // const res:any = await getRepos();
       console.log("The res: ", data);
-      const starredRepos:any = data.user.starredRepositories.edges.map(({ node }: any) => node);
+      const starredRepos:any = data.user.starredRepositories.edges;
       setRepos(starredRepos);
 
       setLoading(false);
