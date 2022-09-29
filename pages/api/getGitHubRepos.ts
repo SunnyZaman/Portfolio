f8bda9c47724bbbf1e409ae6a4bcac8b54aa5963
Sunny Zaman <sunnyjzaman@hotmail.com>
2022-09-29 10:37:54 -0400

    rest api test

--- pages/api/getGitHubRepos.ts
+++ pages/api/getGitHubRepos.ts
@@ -6,35 +6,35 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ) {
-  const client = getApolloClient();
+  // const client = getApolloClient();
 
-  try {
-    const { data } = await client.query({
-      query: gql`
-      {
-        user(login: "sunnyzaman") {
-          starredRepositories(first: 3) {
-            edges {
-              cursor
-              node {
-                id
-                name
-                description
-                languages(first: 5) {
-                  nodes {
-                    name
-                    id
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-      `,
-    });
-    res.status(200).json({ data });
-  } catch (e:any) {
-    res.status(400).json({ error: e.message });
-  }
+  // try {
+  //   const { data } = await client.query({
+  //     query: gql`
+  //     {
+  //       user(login: "sunnyzaman") {
+  //         starredRepositories(first: 3) {
+  //           edges {
+  //             cursor
+  //             node {
+  //               id
+  //               name
+  //               description
+  //               languages(first: 5) {
+  //                 nodes {
+  //                   name
+  //                   id
+  //                 }
+  //               }
+  //             }
+  //           }
+  //         }
+  //       }
+  //     }
+  //     `,
+  //   });
+  //   res.status(200).json({ data });
+  // } catch (e:any) {
+  //   res.status(400).json({ error: e.message });
+  // }
 }
