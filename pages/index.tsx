e638584ffc5aa8f61d6cfcb6dd4889b2f1522bd0
Sunny Zaman <sunnyjzaman@hotmail.com>
2022-09-29 22:31:25 -0400

    octo

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ Props) => {
       try {
         // const octokit = new Octokit();
           const octokit = new Octokit({
-            auth: `${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
+            auth: `token ${process.env.NEXT_PUBLIC_GITHUB_ACCESS_TOKEN}`,
           });
          octokit.graphql(
             `
